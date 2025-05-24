d3c8c7e863f67ad3e77e7f7b583cfd010262cab2
AbW444 <arata.johann@gmail.com>
2025-05-25 00:31:38 +0200

    Delete src directory

--- src/modules/redirect-config.js
+++
@@ -1,9 +0,0 @@
-// URLs à REMPLACER par vos vraies destinations
-export const REDIRECT_URLS = {
-    'grande-barriere': 'https://page-collection-les-ombres-de-la-mer.vercel.app/',
-    'abysses': 'https://page-collection-les-ombres-de-la-mer.vercel.app/',
-    'arctique': 'https://page-collection-les-ombres-de-la-mer.vercel.app/',
-    'plastique': 'https://page-collection-les-ombres-de-la-mer.vercel.app/',
-    'triangle-corail': 'https://page-collection-les-ombres-de-la-mer.vercel.app/',
-    'requins': 'https://page-collection-les-ombres-de-la-mer.vercel.app/'
-};
